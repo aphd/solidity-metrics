58b1182b2e6f6102d01dbf376c03d39a8fca3b7a
apierro <antonio.pierro@gmail.com>
2019-02-25 15:40:33 +0100

    added _get_firstseen_lastseen method

--- examples/write-metrics.py
+++ examples/write-metrics.py
@@ -1,11 +1,29 @@
-import glob, re, subprocess 
+import ast, glob, re, subprocess 
 
 class MetricsAPI(object):
-    """
+    '''
     Base Metrics Api implementation
-    """
+    '''
     def __init__(self):
         pass
+
+    def _append_data_to_each_line(self):
+        pass
+
+    def _get_address(self):
+        ''' get SC address from the file name '''
+        # TODO list comprhnesion in one line of code!!
+        addresses = []
+        for input in glob.glob("./output/*/*.sol"):
+            addresses.append(re.search(r"/([a-z0-9]{40})_", input).group(1))
+        return addresses
+
+    def _get_firstseen_lastseen(self, address):
+        with open('contracts.json', 'r') as f:
+            for line in f:
+                o = ast.literal_eval(line)
+                if o['address'] == address:
+                    return [o['firstseen'], o['lastseen']]
 
     def write_solidity_metrics(self):
         jar = '../target/SolMet-1.0-SNAPSHOT.jar'
@@ -13,6 +31,12 @@ class MetricsAPI(object):
             output = re.sub('.sol$','.out' , input)
             subprocess.call(['java', '-jar', jar, '-inputFile', input, '-outFile', output])
 
+    def append_solidity_metrics(self):
+        ''' It opens the metrics' output generated from SOLMET and append the lastseen and firstseen data generated from EtherChain'''
+        pass
+
+
 if __name__ == "__main__":
     m = MetricsAPI()
-    m.write_solidity_metrics()
+    print(m._get_address())
+    print(m._get_firstseen_lastseen('0xa6e0b24c65758154cac6f33b0c455727ab6193cb'))
