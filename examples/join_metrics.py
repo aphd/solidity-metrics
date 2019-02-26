84a9d13d4434fa3af556fefa9ef950011bdb4efb
apierro <antonio.pierro@gmail.com>
2019-02-26 11:09:53 +0100

    renamed merge-metrics.py to join_metrics.py

--- examples/join_metrics.py
+++ examples/join_metrics.py
@@ -12,24 +12,26 @@ class MergeMetrics(object):
         with open('contracts.json', 'r') as f:
             return [ast.literal_eval(line) for line in f]
 
-    def _get_file_name_from_contract_json_file(self, obj):
+    def get_sol_file_name(self, obj):
             src = os.path.join('./output', obj["address"].replace("0x", "")[:2].lower())
             src = "/".join([src, obj["address"].replace("0x", "")])
-            return "_".join([src, obj["name"] + '.out'])
+            return "_".join([src, obj["name"]])
     
     def _write_file_header(self):
         self.outf = open(self.solmetant, 'w')
-        self.outf.write('SolidityFile;ETHAddress;ContractName;Type;SLOC;LLOC;CLOC;NF;WMC;NL;NLE;NUMPAR;NOS;DIT;NOA;NOD;CBO;NA;NOI;Avg. McCC;Avg. NL;Avg. NLE;Avg. NUMPAR;Avg. NOS;Avg. NOI;FS;LS;')
+        self.outf.write('SolidityFile;ETHAddress;ContractName;Type;SLOC;LLOC;CLOC;NF;WMC;NL;NLE;NUMPAR;NOS;DIT;NOA;NOD;CBO;NA;NOI;Avg. McCC;Avg. NL;Avg. NLE;Avg. NUMPAR;Avg. NOS;Avg. NOI;FS;LS;\n')
 
-    def merge_etherscan_with_solmet(self):
+    def join_etherscan_solmet(self):
         self._write_file_header()
         for obj in self.etherscan_json:
-            fn = self._get_file_name_from_contract_json_file(obj)
+            fn = ".".join([self.get_sol_file_name(obj), 'out'])
+            print('### fn ###', fn)
             lines = open(fn, 'r').readlines()
             print(";".join([lines[1].rstrip(), 'FS', 'LS']))
             self.outf.write(';'.join([lines[1].rstrip(), obj['firstseen'], obj['lastseen'], '\n']))
-        self.outf.close()  
+        self.outf.close()
 
 if __name__ == "__main__":
     m = MergeMetrics()
-    print(m.merge_etherscan_with_solmet())
+    #m.join_etherscan_solmet()
+    print(m.get_sol_file_name({'address': '0x79a64dbe0a25390fa40a2eb819b934ccc7a06f45', 'name': 'ALLDigitalToken', 'compiler': 'Solidity', 'compiler_version': '0.4.25', 'balance': 0, 'txcount': '1', 'firstse    en': '2019-02-26T05:04:06.000Z', 'lastseen': '2019-02-26T05:04:06.000Z'}))
