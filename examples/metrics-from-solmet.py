29f971ab7715a648327a57581d3e84a845032303
apierro <antonio.pierro@gmail.com>
2019-03-30 11:12:52 +0100

    merged examples/.gitignore into .gitignore

--- examples/metrics-from-solmet.py
+++ examples/metrics-from-solmet.py
@@ -1,4 +1,8 @@
-import glob, os, re, subprocess
+import glob
+import os
+import re
+import subprocess
+
 
 class MetricsFromSolMet(object):
     '''
@@ -7,13 +11,14 @@ class MetricsFromSolMet(object):
     Input: ./output/a6/a6e0b24c65758154cac6f33b0c455727ab6193cb_BasicTokenSC.sol
     Output: ./output/a6/a6e0b24c65758154cac6f33b0c455727ab6193cb_BasicTokenSC.out
     '''
+
     def __init__(self):
         pass
 
     def write_solidity_metrics(self):
         jar = '../target/SolMet-1.0-SNAPSHOT.jar'
         for input in glob.glob("./output/*/*.sol"):
-            outFile = re.sub('.sol$','.out' , input)
+            outFile = re.sub('.sol$', '.out', input)
             if not os.path.exists(outFile):
                 print('### ouput file ###: ', outFile)
                 subprocess.call([
